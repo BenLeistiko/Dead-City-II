5dff53dbf63f1fb31a763aa2f7c6ee253d6593c0
Ben <Ben@Ben-Laptop>
2018-05-05 16:52:33 -0700

    supplies

---
+++ Dead City II/src/items/Supplies.java
@@ -0,0 +1,13 @@
+package items;
+
+import interfaces.Drawable;
+import processing.core.PApplet;
+
+public class Supplies implements Drawable {
+
+	@Override
+	public void draw(PApplet marker) {
+
+	}
+
+}
