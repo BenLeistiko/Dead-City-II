25fc28b1366233f437a84d149f3220c83b3bdb2e
Ben <Ben@Ben-Laptop>
2018-05-21 22:36:43 -0700

    instr

--- Dead City II/src/scenes/Instructions.java
+++ Dead City II/src/scenes/Instructions.java
@@ -19,7 +19,7 @@ public class Instructions extends Scene{
 	PImage instr;
 	public Instructions(Main m, Hero joe) {
 		super(m, joe);
-		add(new Button(ASSUMED_DRAWING_WIDTH/2, ASSUMED_DRAWING_HEIGHT-75, 400, 50, "Next Day", new Color(0,0,0), "Main"));
+		add(new Button(ASSUMED_DRAWING_WIDTH/2, ASSUMED_DRAWING_HEIGHT-75, 400, 50, "Main Menu", new Color(0,0,0), "TitleScreen"));
 		instr = Main.resources.getImage("instr");
 	}
 
